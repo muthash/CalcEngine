09464e78640eb6797a6894cd5910967ef4b5a268
muthash <kathingo92@gmail.com>
2018-11-19 21:25:30 +0300

    [Feature] CalcEngine with Classes and Methods

--- src/com/muthama/calcengine/Main.java
+++ src/com/muthama/calcengine/Main.java
@@ -3,40 +3,27 @@ package com.muthama.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        double[] leftVals = {100.0d, 225.0d, 25.0d, 11.0d};
-        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
-        char[] opCodes = {'a', 's', 'm', 'd'};
-        double[] result = new double[opCodes.length];
+        MathEquation[] equations = new MathEquation[4];
 
-       for(int i = 0; i < opCodes.length; i++){
-           switch(opCodes[i]){
-               case 'a':
-                   result[i] = leftVals[i] + rightVals[i];
-                   break;
+        equations[0] = create(100.0d, 50.0d, 'd');
+        equations[1] = create(25.0d, 92.0d, 'a');
+        equations[2] = create(225.0d, 17.0d, 's');
+        equations[3] = create(15.0d, 23.0d, 'm');
 
-               case 's':
-                   result[i] = leftVals[i] - rightVals[i];
-                   break;
-
-               case 'm':
-                   result[i] = leftVals[i] * rightVals[i];
-                   break;
-
-               case 'd':
-                   result[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
-                   break;
-
-               default:
-                   System.out.println("Error - invalid opcode");
-                   result[i] = 0.0d;
-                   break;
-           }
-       }
-
-       for (double theResult : result) {
+        for (MathEquation equation : equations) {
+           equation.execute();
            System.out.print("result = ");
-           System.out.println(theResult);
-       }
+           System.out.println(equation.result);
+        }
 
+    }
+
+    public static MathEquation create (double leftVal, double rightVal, char opCode){
+        MathEquation equation = new MathEquation();
+        equation.leftVal = leftVal;
+        equation.rightVal = rightVal;
+        equation.opCode = opCode;
+
+        return equation;
     }
 }
