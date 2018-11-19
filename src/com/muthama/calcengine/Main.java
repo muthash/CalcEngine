cefba883500d1b57805186f18721e9f2ac769546
muthash <kathingo92@gmail.com>
2018-11-19 20:30:48 +0300

    [Feature] Use arrays and for loop

--- src/com/muthama/calcengine/Main.java
+++ src/com/muthama/calcengine/Main.java
@@ -3,28 +3,34 @@ package com.muthama.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        double val1 = 100;
-        double val2 = 50;
-        double result;
-        char opCode = 'd';
+        double[] leftVals = {100.0d, 225.0d, 25.0d, 11.0d};
+        double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+        char[] opCodes = {'a', 's', 'm', 'd'};
+        double[] result = new double[opCodes.length];
 
-        if(opCode =='a')
-            result = val1 + val2;
-        else if(opCode =='s')
-            result = val1 - val2;
-        else if(opCode =='m')
-            result = val1 * val2;
-        else if(opCode =='d') {
-            result = val2 != 0.0d ? val1 / val2 : 0.0d;
-//            if (val2 != 0.0d)
-//                result = val1 / val2;
-//            else
-//                result = 0.0d;
-        }
-        else {
-            System.out.println("Error - invalid opcode");
-            result = 0.0d;
-        }
-        System.out.print(result);
+       for (int i = 0; i < opCodes.length; i++){
+           if(opCodes[i] =='a')
+               result[i] = leftVals[i] + rightVals[i];
+
+           else if(opCodes[i] =='s')
+               result[i] = leftVals[i] - rightVals[i];
+
+           else if(opCodes[i] =='m')
+               result[i] = leftVals[i] * rightVals[i];
+
+           else if(opCodes[i] =='d')
+               result[i] = rightVals[i] != 0.0d ? leftVals[i] / rightVals[i] : 0.0d;
+           
+           else {
+               System.out.println("Error - invalid opcode");
+               result[i] = 0.0d;
+           }
+
+       }
+       for (double theResult : result) {
+           System.out.print("result = ");
+           System.out.println(theResult);
+       }
+
     }
 }
