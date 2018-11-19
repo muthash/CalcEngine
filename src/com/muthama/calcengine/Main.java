928e055352790888c2bdeccfc86c720cc76142b0
muthash <kathingo92@gmail.com>
2018-11-19 20:02:17 +0300

    [Feature] Simple calc engine using conditional statements

--- src/com/muthama/calcengine/Main.java
+++ src/com/muthama/calcengine/Main.java
@@ -3,6 +3,28 @@ package com.muthama.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        double val1 = 100;
+        double val2 = 50;
+        double result;
+        char opCode = 'd';
+
+        if(opCode =='a')
+            result = val1 + val2;
+        else if(opCode =='s')
+            result = val1 - val2;
+        else if(opCode =='m')
+            result = val1 * val2;
+        else if(opCode =='d') {
+            result = val2 != 0.0d ? val1 / val2 : 0.0d;
+//            if (val2 != 0.0d)
+//                result = val1 / val2;
+//            else
+//                result = 0.0d;
+        }
+        else {
+            System.out.println("Error - invalid opcode");
+            result = 0.0d;
+        }
+        System.out.print(result);
     }
 }
