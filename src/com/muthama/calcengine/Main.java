eafb74564d619f55cccecf86b6ba45922357bb85
muthash <kathingo92@gmail.com>
2018-11-20 15:57:38 +0300

    [Feature] CalcEngine with String Data Type Capabilities

--- src/com/muthama/calcengine/Main.java
+++ src/com/muthama/calcengine/Main.java
@@ -3,54 +3,68 @@ package com.muthama.calcengine;
 public class Main {
 
     public static void main(String[] args) {
-        MathEquation[] equations = new MathEquation[4];
-
-        equations[0] = new MathEquation('d', 100.0d, 50.0d);
-        equations[1] = new MathEquation('a', 25.0d, 92.0d);
-        equations[2] = new MathEquation('s', 225.0d, 17.0d);
-        equations[3] = new MathEquation('m', 15.0d, 23.0d);
-
-        for (MathEquation equation : equations) {
-           equation.execute();
-           System.out.print("result = ");
-           System.out.println(equation.getResult());
-        }
-
-        System.out.println();
-        System.out.println("Using Overloading");
-        System.out.println();
-
-        double leftDouble = 9.0d;
-        double rightDouble = 4.0d;
-        int leftInt = 9;
-        int rightInt = 4;
-
-        MathEquation equationOverload = new MathEquation('d');
-
-        equationOverload.execute(leftDouble, rightDouble);
-        System.out.print("result = ");
-        System.out.println(equationOverload.getResult());
-
-        equationOverload.execute(leftInt, rightInt);
-        System.out.print("integer result = ");
-        System.out.println(equationOverload.getResult());
-
-        System.out.println();
-        System.out.println("Using Inheritance");
-        System.out.println();
-
-        CalculateBase[] calculators = {
-                new Divider (100.0d, 50.0d),
-                new Adder (100.0d, 50.0d),
-                new Multiplier (100.0d, 50.0d),
-                new Subtractor (100.0d, 50.0d)
+        String[] statements = {
+                "divide 100.0 50.0",
+                "add 100.0 50.0",
+                "multiply 100.0 50.0",
+                "subtract 100.0 50.0"
         };
 
-        for (CalculateBase calculator: calculators) {
-            calculator.calculate();
-            System.out.print("result = ");
-            System.out.println(calculator.getResult());
+        CalculateHelper helper = new CalculateHelper();
+        for(String statement: statements){
+            helper.process(statement);
+            System.out.println(helper);
         }
+
+//
+//        MathEquation[] equations = new MathEquation[4];
+//
+//        equations[0] = new MathEquation('d', 100.0d, 50.0d);
+//        equations[1] = new MathEquation('a', 25.0d, 92.0d);
+//        equations[2] = new MathEquation('s', 225.0d, 17.0d);
+//        equations[3] = new MathEquation('m', 15.0d, 23.0d);
+//
+//        for (MathEquation equation : equations) {
+//           equation.execute();
+//           System.out.print("result = ");
+//           System.out.println(equation.getResult());
+//        }
+//
+//        System.out.println();
+//        System.out.println("Using Overloading");
+//        System.out.println();
+//
+//        double leftDouble = 9.0d;
+//        double rightDouble = 4.0d;
+//        int leftInt = 9;
+//        int rightInt = 4;
+//
+//        MathEquation equationOverload = new MathEquation('d');
+//
+//        equationOverload.execute(leftDouble, rightDouble);
+//        System.out.print("result = ");
+//        System.out.println(equationOverload.getResult());
+//
+//        equationOverload.execute(leftInt, rightInt);
+//        System.out.print("integer result = ");
+//        System.out.println(equationOverload.getResult());
+//
+//        System.out.println();
+//        System.out.println("Using Inheritance");
+//        System.out.println();
+//
+//        CalculateBase[] calculators = {
+//                new Divider (100.0d, 50.0d),
+//                new Adder (100.0d, 50.0d),
+//                new Multiplier (100.0d, 50.0d),
+//                new Subtractor (100.0d, 50.0d)
+//        };
+//
+//        for (CalculateBase calculator: calculators) {
+//            calculator.calculate();
+//            System.out.print("result = ");
+//            System.out.println(calculator.getResult());
+//        }
 
     }
 }
---
+++ src/com/muthama/calcengine/MathCommand.java
@@ -0,0 +1,8 @@
+package com.muthama.calcengine;
+
+public enum MathCommand {
+    Add,
+    Subtract,
+    Multiply,
+    Divide
+}
